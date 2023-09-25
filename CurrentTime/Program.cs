84ed620417e2546863dc50e7f003c46e2e2a3681
zairulmazwan <zairulmazwan@gmail.com>
2023-09-25 21:01:36 +0100

    v1.1

--- CurrentTime/Program.cs
+++ CurrentTime/Program.cs
@@ -1,5 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
+﻿
 Console.WriteLine("This program is an example how to capture timestamp and to get the duration");
 timer.get_duration_example();
